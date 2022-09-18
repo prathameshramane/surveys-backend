b9fe9b5101505f72776a9eb1f47aafa103758bd8
Prathamesh Ramane <Prathamesh.Ramane@lntinfotech.com>
2022-09-18 21:28:18 +0530

    Updated static URL

--- backend/urls.py
+++ backend/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.views.static import serve
+from django.urls import path, include, re_path
+
+from backend import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/surveys/', include('surveys.urls'), name='survey-urls'),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
